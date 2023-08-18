96a11cd3ee3b506d268c8706cdd1b24c05c4c765
old-coder77 <141748796+old-coder77@users.noreply.github.com>
2023-08-17 23:00:53 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,67 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  console.log(isNaN(isbn));
+  console.log(isbn);
+  if (!isNaN(isbn)) {
+        const book = books[isbn];
+       
+        if (book) {
+            return res.send(JSON.stringify(book));
+        } else {
+            return res.status(300).json({message: "Sorry, we do  not carry this book"});
+        } 
+    }else {
+        return res.status(300).json({message: "Please enter a number"});
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+  const keys = Object.keys(books);
+  var bookSend = [];
+
+  for (let i = 0; i < keys.length; i++) {
+      const book = books[keys[i]];
+      if (book.author === author) {
+          bookSend.push(book);
+      }
+    }
+
+    
+    if (bookSend.length === 0) {
+        return res.status(300).json({message: "We do not carry any books by this author"});
+    } else {
+        return res.send(JSON.stringify(bookSend));
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const keys = Object.keys(books);
+    var bookSend = [];
+  
+    for (let i = 0; i < keys.length; i++) {
+        const book = books[keys[i]]; 
+        if (book.title === title) {
+            bookSend.push(book);
+        }
+      }
+  
+      
+      if (bookSend.length === 0) {
+          return res.status(300).json({message: "We do not carry any books by this author"});
+      } else {
+          return res.send(JSON.stringify(bookSend));
+      }
 });
 
 //  Get book review
